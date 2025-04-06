a173dfde2ccbb3ea92aa812276d964d55fb98a0c
Inbal Horo <inbal.peer317@gmail.com>
2025-04-06 13:36:35 -0700

    SHOP not finished & BAG

--- app/src/main/java/com/horovitz/memorygame/MainShop.java
+++ app/src/main/java/com/horovitz/memorygame/MainShop.java
@@ -17,7 +17,7 @@ public class MainShop extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_shop);  // ודא שה-XML שלך נקרא נכון
-
+//GJIJG
     }
 //        // איפוס המידע
 //        layout = findViewById(R.id.layout);  // מתחבר ל-LinearLayout הפנימי ב-ScrollView
